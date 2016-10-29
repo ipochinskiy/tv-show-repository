c215a4bcd573558bb395341db2844e70179e0b41
Pochinskii Ilia <zorkiypnz@yandex.ru>
2016-10-29 04:28:46 +0200

    added some npm scripts

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
 
-gulp.task('sass', function() {
-  gulp.src('public/stylesheets/style.scss')
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(gulp.dest('public/stylesheets'));
-});
+gulp.task('sass', () => gulp
+  .src('public/stylesheets/**/*.scss')
+  .pipe(plumber())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('public/stylesheets'))
+);
 
 gulp.task('watch', () => gulp.watch('public/stylesheets/*.scss', ['sass']));
 
