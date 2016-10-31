fff4d64d142bbed5184143cb08a8fd9ee3e89c09
Pochinskii Ilia <zorkiypnz@yandex.ru>
2016-10-31 02:00:43 +0100

    added view and controller for a show addition page

---
+++ public/controllers/add.js
@@ -0,0 +1,26 @@
+angular.module('MyApp')
+  .controller('AddCtrl', ['$scope', '$alert', 'Show', function($scope, $alert, Show) {
+    $scope.addShow = () => {
+      Show.save({ showName: $scope.showName },
+        () => {
+          $scope.showName = '';
+          $scope.addForm.$setPristine();
+          $alert({
+            content: 'TV show has been added.',
+            placement: 'top-right',
+            type: 'success',
+            duration: 3
+          });
+        },
+        response => {
+          $scope.showName = '';
+          $scope.addForm.$setPristine();
+          $alert({
+            content: response.data.message,
+            placement: 'top-right',
+            type: 'danger',
+            duration: 3
+          });
+        });
+    };
+  }]);
