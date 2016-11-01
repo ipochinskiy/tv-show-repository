274b5663ea7767b70a50f2483c087c6e84ecdd08
Pochinskii Ilia <zorkiypnz@yandex.ru>
2016-11-01 11:30:13 +0100

    used native promises for mongoose

--- src/server.js
+++ src/server.js
@@ -11,11 +11,11 @@ const routes = require('./routes');
 const baseDir = path.join(__dirname, '..');
 const publicDir = path.join(baseDir, 'public');
 
+mongoose.Promise = global.Promise;
 mongoose.connect('localhost');
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
@@ -23,14 +23,12 @@ app.use(cookieParser());
 app.use(express.static(publicDir));
 
 routes.forEach(route =>
-  app[route.method](route.endpoint, route.action));
+	app[route.method](route.endpoint, route.action));
 
 app.use((err, req, res) => {
-  console.error(err.stack);
-  res.status(500).send({ message: err.message });
+	console.error(err.stack);
+	res.status(500).send({ message: err.message });
 });
 
-app.listen(
-  app.get('port'),
-  () => console.log(`Express server listening on port ${app.get('port')}`)
-);
+app.listen(process.env.PORT || 3000, () =>
+	console.log(`Express server listening on port ${app.get('port')}`));
