3d85ea019f2c81effe67e1f7e47c54d8f3650b1d
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-10 00:05:56 +0100

    added css optimizer to the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
+const csso = require('gulp-csso');
 
 gulp.task('sass', () => gulp
   .src('public/stylesheets/**/*.scss')
   .pipe(plumber())
   .pipe(sass().on('error', sass.logError))
+  .pipe(csso())
   .pipe(gulp.dest('public/stylesheets'))
 );
 
