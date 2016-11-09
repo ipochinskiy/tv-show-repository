3536d901479fd5a67caf59d4a51daf8cf4c0ac05
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-10 00:11:16 +0100

    added watching js files changes to the `watch` task of the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const plumber = require('gulp-plumber');
 const csso = require('gulp-csso');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
+const templateCache = require('gulp-angular-templatecache');
 
 gulp.task('sass', () => gulp
   .src('public/stylesheets/**/*.scss')
@@ -13,7 +14,6 @@ gulp.task('sass', () => gulp
   .pipe(gulp.dest('public/stylesheets'))
 );
 
-gulp.task('watch', () => gulp.watch('public/stylesheets/*.scss', ['sass']));
 gulp.task('compress', function() {
   gulp.src([
     'public/vendor/angular.js',
@@ -27,6 +27,17 @@ gulp.task('compress', function() {
     .pipe(concat('app.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('public'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('public/stylesheets/*.scss', [ 'sass' ]);
+    gulp.watch('public/views/**/*.html', [ 'templates' ]);
+    gulp.watch([
+        'public/**/*.js',
+        '!public/app.min.js',
+        '!public/templates.js',
+        '!public/vendor'
+    ], [ 'compress' ]);
 });
 
 gulp.task('default', [ 'sass', 'compress', 'watch' ]);
