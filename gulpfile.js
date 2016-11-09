513b915b13726d6e70a490ea6551c7280cf9da90
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-10 00:10:01 +0100

    added compressing js files to the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
 const csso = require('gulp-csso');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 
 gulp.task('sass', () => gulp
   .src('public/stylesheets/**/*.scss')
@@ -12,5 +14,19 @@ gulp.task('sass', () => gulp
 );
 
 gulp.task('watch', () => gulp.watch('public/stylesheets/*.scss', ['sass']));
+gulp.task('compress', function() {
+  gulp.src([
+    'public/vendor/angular.js',
+    'public/vendor/*.js',
+    'public/app.js',
+    'public/services/*.js',
+    'public/controllers/*.js',
+    'public/filters/*.js',
+    'public/directives/*.js'
+  ])
+    .pipe(concat('app.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public'));
+});
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', [ 'sass', 'compress', 'watch' ]);
