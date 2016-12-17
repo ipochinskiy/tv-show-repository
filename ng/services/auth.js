034804a899b68a75e8fb34d4c8aef70bd11ccda7
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-12-17 14:17:08 +0100

    removed an parameters array from the auth factory

--- ng/services/auth.js
+++ ng/services/auth.js
@@ -1,69 +1,70 @@
-angular.module('MyApp')
-	.factory('Auth', ['$http', '$location', '$rootScope', '$cookieStore', '$alert',
-		function ($http, $location, $rootScope, $cookieStore, $alert) {
-			$rootScope.currentUser = $cookieStore.get('user');
-			$cookieStore.remove('user');
+angular.module('MyApp').factory(
+	'Auth',
+	function ($http, $location, $rootScope, $cookieStore, $alert) {
+		$rootScope.currentUser = $cookieStore.get('user');
+		$cookieStore.remove('user');
 
-			return {
-				login(user) {
-					return $http.post('/api/login', user)
-						.success((data) => {
-							$rootScope.currentUser = data;
-							$location.path('/');
+		return {
+			login(user) {
+				return $http.post('/api/login', user)
+					.success((data) => {
+						$rootScope.currentUser = data;
+						$location.path('/');
 
-							$alert({
-								title: 'Cheers!',
-								content: 'You have successfully logged in.',
-								placement: 'top-right',
-								type: 'success',
-								duration: 3,
-							});
-						})
-						.error(() => {
-							$alert({
-								title: 'Error!',
-								content: 'Invalid username or password.',
-								placement: 'top-right',
-								type: 'danger',
-								duration: 3,
-							});
+						$alert({
+							title: 'Cheers!',
+							content: 'You have successfully logged in.',
+							placement: 'top-right',
+							type: 'success',
+							duration: 3,
 						});
-				},
-				signup(user) {
-					return $http.post('/api/signup', user)
-						.success(() => {
-							$location.path('/login');
+					})
+					.error(() => {
+						$alert({
+							title: 'Error!',
+							content: 'Invalid username or password.',
+							placement: 'top-right',
+							type: 'danger',
+							duration: 3,
+						});
+					});
+			},
+			signup(user) {
+				return $http.post('/api/signup', user)
+					.success(() => {
+						$location.path('/login');
 
-							$alert({
-								title: 'Congratulations!',
-								content: 'Your account has been created.',
-								placement: 'top-right',
-								type: 'success',
-								duration: 3,
-							});
-						})
-						.error((response) => {
-							$alert({
-								title: 'Error!',
-								content: response.data,
-								placement: 'top-right',
-								type: 'danger',
-								duration: 3,
-							});
+						$alert({
+							title: 'Congratulations!',
+							content: 'Your account has been created.',
+							placement: 'top-right',
+							type: 'success',
+							duration: 3,
 						});
-				},
-				logout() {
-					return $http.get('/api/logout')
-						.success(() => {
-							$rootScope.currentUser = null;
-							$cookieStore.remove('user');
-							$alert({
-								content: 'You have been logged out.',
-								placement: 'top-right',
-								type: 'info',
-								duration: 3,
-							});
+					})
+					.error((response) => {
+						$alert({
+							title: 'Error!',
+							content: response.data,
+							placement: 'top-right',
+							type: 'danger',
+							duration: 3,
 						});
-				},
-			};
-		}]);
+					});
+			},
+			logout() {
+				return $http.get('/api/logout')
+					.success(() => {
+						$rootScope.currentUser = null;
+						$cookieStore.remove('user');
+						$alert({
+							content: 'You have been logged out.',
+							placement: 'top-right',
+							type: 'info',
+							duration: 3,
+						});
+					});
+			},
+		};
+	}
+);
