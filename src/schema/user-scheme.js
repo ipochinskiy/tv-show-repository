5060f3b8285181444a0b6070a9f07c235a46e55d
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-12-17 02:40:04 +0100

    added properties for FB authentication to the user scheme

--- src/schema/user-scheme.js
+++ src/schema/user-scheme.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
 	email: { type: String, unique: true },
 	password: String,
+	facebook: String,
+	firstName: String,
+	lastName: String,
 });
 
 const User = mongoose.model('User', userSchema);
