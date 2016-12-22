8a52b800df31d873c49514ce18ec3bb75491cbe8
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-12-22 23:51:18 +0100

    added google auth support to the auth-controller, extracted #authSocial() function and generalized it

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,5 +1,24 @@
  /* eslint-disable arrow-body-style */
 exports.makeAuthController = ({ auth, userModel }) => {
+
+	function authSocial(authMethod, profile) {
+		return userModel.findOne({ `${authMethod}.id`: profile.id })
+			.then((existingUser) => {
+				let newUser;
+				if (!existingUser) {
+					newUser = userModel.createOne({
+						name: profile.name,
+						[authMethod]: {
+							id: profile.id,
+							email: profile.email,
+						},
+					});
+				}
+				const token = auth.createJwtToken(existingUser || newUser);
+				return { ok: true, token };
+			});
+	}
+
 	return {
 		signup: (name, email, password) => userModel.createUser({ name, email, password }),
 
@@ -15,22 +34,9 @@ exports.makeAuthController = ({ auth, userModel }) => {
 			if (signature !== auth.getFacebookSignature(payload)) {
 				return Promise.reject({ invalidSignature: true });
 			}
-
-			return userModel.findOne({ facebook: profile.id })
-				.then((existingUser) => {
-					let newUser;
-					if (!existingUser) {
-						newUser = userModel.createOne({
-							name: profile.name,
-							facebook: {
-								id: profile.id,
-								email: profile.email,
-							},
-						});
-					}
-					const token = auth.createJwtToken(existingUser || newUser);
-					return { ok: true, token };
-				});
+			return authSocial('facebook', profile);
 		},
+
+		google: (profile) => authSocial('google', profile),
 	};
 };
