76f9c6a212c0389251ab3aecefa525b2e7305478
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-12-31 00:33:17 +0100

    added a simple test for the Auth service

---
+++ test/ng/services.spec.js
@@ -0,0 +1,10 @@
+describe('service', function () {
+	beforeEach(module('MyApp'));
+
+	describe('Auth', function () {
+		it('should return current version', inject(function (Auth) {
+
+			expect(Auth).toBeDefined();
+		}));
+	});
+});
